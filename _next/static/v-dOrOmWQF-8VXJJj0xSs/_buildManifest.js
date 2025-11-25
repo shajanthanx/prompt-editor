b68d683e8de3fa7940e9879cbfc91e3997fe4cec
shajanthan <shajanthan@codegen.net>
2025-11-25 17:43:56 +0530

    Updates

--- _next/static/v-dOrOmWQF-8VXJJj0xSs/_buildManifest.js
+++ _next/static/v-dOrOmWQF-8VXJJj0xSs/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/prompt-editor//_next/:path+",
+        "destination": "/prompt-editor/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
