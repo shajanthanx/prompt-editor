bbd92ee2b39465f6aa2c23d66521779e0d83935c
shajanthan <shajanthan@codegen.net>
2026-02-09 13:15:36 +0530

    Build and deploy site

---
+++ out/_next/static/i4SIU3viChGAjzL53qkIO/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/prompt-editor//_next/:path+",
+        "destination": "/prompt-editor/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
